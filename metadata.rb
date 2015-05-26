59db7122cc8f67d5193c52c1f4b2b15958b8d47d
Michael Belt <michael@mdbelt.com>
2015-05-26 10:59:47 -0500

    Initial Cut from Knife generation

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mongodb-tar'
+maintainer       'Michael Belt'
+maintainer_email 'michael@mdbelt.com'
+license          'Apache 2.0'
+description      'Installs/Configures mongodb-tar'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: mongodb-tar
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#

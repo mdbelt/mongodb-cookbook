19dcc8ff86009c99bb48f3195039db03e1ecd408
Michael Belt <michael@mdbelt.com>
2015-05-28 16:51:49 -0500

    Re-worked shell code to be more modular

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -1,13 +1,22 @@
 Chef::Log.warn("**********************Begin Repl Dev Block")
 
-
-execute "/opt/mongodb/bin/mongo #{node['ipaddress']}:27017 -eval 'printjson(rs.initiate())'" do
-  only_if do
-    #Only Initiate if rs.status() returns error code 94.
-    cmd = Mixlib::ShellOut.new("/opt/mongodb/bin/mongo #{node['ipaddress']}:27017 -eval 'rs.status()[\"code\"]'")
+ruby_block "Does_Current_Need_Repl_Work" do
+  block do
+    cmd = Mixlib::ShellOut.new("/opt/mongodb/bin/mongo #{node['ipaddress']}:27017 --quiet --eval 'rs.status()[\"code\"]'")
     status = cmd.run_command.stdout.strip
-    status.end_with?("94")
+    if status.eql?("94")
+      notifies :run, resources(:ruby_block => "Initiate_Current_Node"), :immediately
+    end
   end
 end
+
+ruby_block "Initiate_Current_Node" do
+  block do
+    Mixlib::ShellOut.new("/opt/mongodb/bin/mongo #{node['ipaddress']}:27017 --quiet --eval 'printjsononeline(rs.initiate())'").run_command
+  end
+  action :nothing
+end
+
+#rs.status().members.forEach(function(z){if(z.stateStr == "PRIMARY")printjson(z.name);})
 
 Chef::Log.warn("**********************End Repl Dev Block")
